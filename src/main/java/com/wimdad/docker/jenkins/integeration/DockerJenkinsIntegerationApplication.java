31a72dc07dd2c63142c9218a197e23d7b9dc8ad7
Waquar Imdad <wimdad@Waquars-Laptop.local>
2024-01-03 15:56:59 +0530

    first commit

---
+++ src/main/java/com/wimdad/docker/jenkins/integeration/DockerJenkinsIntegerationApplication.java
@@ -0,0 +1,13 @@
+package com.wimdad.docker.jenkins.integeration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerJenkinsIntegerationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DockerJenkinsIntegerationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wimdad/docker/jenkins/integeration/DockerJenkinsIntegerationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wimdad.docker.jenkins.integeration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerJenkinsIntegerationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
